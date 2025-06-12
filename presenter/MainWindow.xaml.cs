9e43b311a0e9c6f8b48c2703428b27ce120d5b04
Xilobone <xilobone@gmail.com>
2025-06-12 20:41:43 +0200

    Added spinning n-sided polygon

--- presenter/MainWindow.xaml.cs
+++ presenter/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿//using System.Drawing;
+using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace presenter
 {
@@ -16,23 +19,76 @@ namespace presenter
     /// </summary>
     public partial class MainWindow : Window
     {
+        float rotation = 0;
+        float rotationSpeed = 0.05f; //rotations per second
 
         public MainWindow()
         {
             InitializeComponent();
+            CompositionTarget.Rendering += OnRendering;
+            CreateShape();
 
-            progressBar.Value = 0;
-
-            IncrementProgressBar();
+            StartTimer();
         }
 
-        private async void IncrementProgressBar()
-        {
-            for (int i = 0; i <= 100; i++)
+        void StartTimer()
+        {   
+            float updateInterval = 1000f / 60f;
+            DispatcherTimer timer = new DispatcherTimer
             {
-                progressBar.Value = i;
-                await Task.Delay(50);
+                Interval = TimeSpan.FromMilliseconds(updateInterval)
+            };
+            timer.Tick += (sender, e) =>
+            {
+                rotation += rotationSpeed * (float)(Math.PI * 2) / 60f;
+            };
+            timer.Start();
+        }
+
+        private void OnRendering(object? sender, EventArgs e)
+        {
+            canvas.Children.Clear();
+            CreateShape();
+
+        }
+
+        void CreateShape()
+        {
+            int radius = 300;
+            int segments = 50;
+            Point center = new Point(canvas.Width / 2, canvas.Height / 2);
+
+            for (int i = 0; i < segments; i++)
+            {
+                double angle = (i * 2 * Math.PI) / segments;
+                double angle2 = ((i + 1) * 2 * Math.PI) / segments;
+
+                Point point1 = new Point(center.X + radius * Math.Cos(angle + rotation), center.Y + radius * Math.Sin(angle + rotation));
+                Point point2 = new Point(center.X + radius * Math.Cos(angle2 + rotation), center.Y + radius * Math.Sin(angle2 + rotation));
+
+                PathFigure figure = new PathFigure
+                {
+                    StartPoint = center,
+                    IsClosed = true
+                };
+
+                figure.Segments.Add(new LineSegment(point1, true));
+                figure.Segments.Add(new LineSegment(point2, true));
+
+                PathGeometry geometry = new PathGeometry();
+                geometry.Figures.Add(figure);
+
+                Path path = new Path
+                {
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 3,
+                    Data = geometry
+                };
+
+                canvas.Children.Add(path);
             }
         }
+
+
     }
 }
